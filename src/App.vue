bffa2bae462a0d05bce30047d82b1b12d0d4454b
Leandro de Lima <leandrolima@cobrape.com.br>
2023-02-09 14:07:26 -0300

    Criado componente BlogCard

--- src/App.vue
+++ src/App.vue
@@ -101,4 +101,31 @@ export default Vue.extend({
     fill: #fff;
   }
 }
+
+.blog-card-wrapper{
+  position: relative;
+  padding: 80px 16px;
+  background-color: #f1f1f1;
+  @media(min-width: 500px){
+    padding: 100px 16px;
+  }
+
+  .blog-cards{
+    display: grid;
+    gap: 32px;
+    grid-template-columns: 1fr;
+    @media(min-width: 500px){
+      grid-template-columns: repeat(2, 1fr)
+    }
+    @media(min-width: 900px){
+      grid-template-columns: repeat(3, 1fr)
+    }
+    @media(min-width: 1200px){
+      grid-template-columns: repeat(4, 1fr)
+    }
+
+
+  }
+
+}
 </style>
